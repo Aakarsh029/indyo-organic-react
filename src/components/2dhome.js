313a874b24c1101e62fca349eeb821f94da4a03d
Aakarsh <20bcs007@nith.ac.in>
2022-07-18 16:19:43 +0530

    slight update

--- src/components/2dhome.js
+++ src/components/2dhome.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div
-      id="carouselExampleIndicators"
+      id="carouselExampleIndicators" 
       className="carousel slide"
       data-ride="carousel"
       style={{
